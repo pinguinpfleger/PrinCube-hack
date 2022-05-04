e33f527de84fdcfb97bdc9a2d72c3b7afe0e3f67
pinguinpfleger <kai@pinguinpfleger.de>
2022-05-04 20:38:04 +0200

    little bit Readme enhancements gzipped all web files to save storage removed .map files since they are not needed in production activated WiFi client mode by default

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # PrinCube-hack
 
-mods, improvements, hacks for the PrinCube based on PrinCube-fw-4.33_4.16
-https://github.com/dukelec/mb/releases/tag/4.33_4.16
+mods, improvements, hacks for the PrinCube based on APP 4.36, FW 4.17 released 30 Dec 2021
+https://github.com/dukelec/mb/releases/tag/4.36_4.17
 https://github.com/dukelec/mb
 
 ## Added so far:
+
 wifi client mode
 simple html shell
 
 ## playing as root with webshell without firmware modification
-shell.html uses the usb ip by default
 
+Connect PrinCube with USB to your PC.
+Open shell.html in your browser and start hacking. maybe start with typing: `whoami`
+shell.html uses the usb ip (192.168.88.1) by default
 
 ## Install:
 
@@ -19,38 +22,52 @@ and add a html webshell with root rights under /shell.html
 
 1. Download this FW Mod
 2. Edit mb_etc/wpa_client.conf to your needs
-3. create firmware tar file (on Windows use 7-zip, built-in tar does not set execute bit on run.sh correct)
+3. Create firmware tar file (on Windows use 7-zip, built-in tar does not set execute bit on run.sh correct)
+
 ```
-tar cf princube-fw-mod0.1.tar mbrush-fw/
+tar cf princube-hack.tar mbrush-fw/
 ```
-4. upload and install with webinterface
+
+4. Zpload and install with webinterface
+   When connected via USB is has the IP 192.168.88.1
+   http://192.168.88.1/
 
 ## switch between client and ap mode
 
-disable wifi client mode and enable ap mode
+Disable wifi client mode and enable ap mode
+
 ```
 rm /root/mb_conf/wifi_client  && reboot
 ```
-enable wifi client mode and disable ap mode
+
+Enable wifi client mode and disable ap mode
+
 ```
 touch /root/mb_conf/wifi_client && reboot
 ```
 
+To check which Wifi IP Address PrinCube got
+
+```
+ip a
+```
+
 ## TODO:
+
 security!!! user roles (admin and user)  
 ntpdate  
 wifi ap fallback when wpa client mode fails  
-wifi config webinterface    
+wifi config webinterface  
 dropbear?  
-webui improvments   
-...   
-  
-Contributions are welcome :-) 
- 
-## usefull Links 
+webui improvments  
+...
 
-https://blog.d-l.io/mb-faq 
+Contributions are welcome :-)
 
-## compatible cartriges 
+## usefull Links
 
-https://github.com/dukelec/mb/issues/12 
+https://blog.d-l.io/mb-faq
+
+## compatible cartriges
+
+https://github.com/dukelec/mb/issues/12
--- mbrush-fw/run.sh
+++ mbrush-fw/run.sh
@@ -5,38 +5,27 @@
 mv mbrush-fw/mb_etc/wpa_client.conf /root/mb_etc/wpa_client.conf
 
 # activate wifi_client mode, see /root/mb_etc/S80mb script
-#[ ! -f /root/mb_conf/wifi_client ] && touch /root/mb_conf/wifi_client
+[ ! -f /root/mb_conf/wifi_client ] && touch /root/mb_conf/wifi_client
 
-# cp index.html with MOD Title
-[ -f /root/mb_ser/index.html ] && rm /root/mb_ser/index.html 
-mv mbrush-fw/mb_ser/index.html /root/mb_ser/index.html 
+# cp index.html.gz with MOD Title
+[ -f /root/mb_ser/index.html.gz ] && rm /root/mb_ser/index.html.gz 
+mv mbrush-fw/mb_ser/index.html.gz /root/mb_ser/index.html.gz 
 
+# cp shell.html.gz
+[ -f /root/mb_ser/shell.html.gz ] && rm /root/mb_ser/shell.html.gz 
+mv mbrush-fw/mb_ser/shell.html.gz /root/mb_ser/shell.html.gz 
 
-# cp shell.html
-[ -f /root/mb_ser/shell.html ] && rm /root/mb_ser/shell.html 
-mv mbrush-fw/mb_ser/shell.html /root/mb_ser/shell.html 
+# cp jquery.terminal.min.css.gz
+[ -f /root/mb_ser/jquery.terminal.min.css.gz ] && rm /root/mb_ser/jquery.terminal.min.css.gz
+mv mbrush-fw/mb_ser/jquery.terminal.min.css.gz /root/mb_ser/jquery.terminal.min.css.gz
 
-# cp jquery.terminal.min.css
-[ -f /root/mb_ser/jquery.terminal.min.css ] && rm /root/mb_ser/jquery.terminal.min.css
-mv mbrush-fw/mb_ser/jquery.terminal.min.css /root/mb_ser/ 
+# cp jquery.terminal.min.js.gz
+[ -f /root/mb_ser/jquery.terminal.min.js.gz ] && rm /root/mb_ser/jquery.terminal.min.js.gz
+mv mbrush-fw/mb_ser/jquery.terminal.min.js.gz /root/mb_ser/jquery.terminal.min.js.gz
 
-# cp jquery.terminal.min.css.map
-[ -f /root/mb_ser/jquery.terminal.min.css.map ] && rm /root/mb_ser/jquery.terminal.min.css.map
-mv mbrush-fw/mb_ser/jquery.terminal.min.css.map /root/mb_ser/ 
-
-
-# cp jquery.terminal.min.js
-[ -f /root/mb_ser/jquery.terminal.min.js ] && rm /root/mb_ser/jquery.terminal.min.js
-mv mbrush-fw/mb_ser/jquery.terminal.min.js /root/mb_ser/ 
-
-# cp jquery.terminal.min.js.map
-[ -f /root/mb_ser/jquery.terminal.min.js.map ] && rm /root/mb_ser/jquery.terminal.min.js.map
-mv mbrush-fw/mb_ser/jquery.terminal.min.js.map /root/mb_ser/ 
-
-
-# cp jquery-3.3.1.min.js
-[ -f /root/mb_ser/jquery-3.3.1.min.js ] && rm /root/mb_ser/jquery-3.3.1.min.js
-mv mbrush-fw/mb_ser/jquery-3.3.1.min.js /root/mb_ser/ 
+# cp jquery-3.3.1.min.js.gz
+[ -f /root/mb_ser/jquery-3.3.1.min.js.gz ] && rm /root/mb_ser/jquery-3.3.1.min.js.gz
+mv mbrush-fw/mb_ser/jquery-3.3.1.min.js.gz /root/mb_ser/jquery-3.3.1.min.js.gz
 
 # replace S80mb script 
 [ -f /root/mb_etc/S80mb ] && rm /root/mb_etc/S80mb
